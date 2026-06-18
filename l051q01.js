e1b6522fa63279833edae2b41022ac0784f34cf2
Kassya <kassyabrito2014@gmail.com>
2026-06-18 11:52:18 -0300

    Fim da aula

--- l051q01.js
+++ l051q01.js
@@ -47,16 +47,29 @@
 
 // Questão 5
 
-let n1 = 989;
-let cont = 1;
-let res;
+// let n1 = 989;
+// let cont = 1;
+// let res;
 
 
-while(cont<=10){
+// while(cont<=10){
     
-    res = cont * n1 
+//     res = cont * n1 
    
-    console.log(`${n1} x ${cont} = ${res}`)  
-    cont = cont + 1;
+//     console.log(`${n1} x ${cont} = ${res}`)  
+//     cont = cont + 1;
     
-}
+// }
+
+//Questão 6 (FAZER EM CASA)
+/*Desenvolver um programa que leia um número n qualquer menor ou igual a 50 e apresente o valor obtido da
+multiplicação sucessiva de n por 3 enquanto o produto for menor que 250. (n x 3; n x 3 x 3; n x 3 x 3 x 3 etc...).
+*/
+
+
+
+
+//Questão 7 (FAZER EM CASA)
+
+
+
---
+++ l052q01.js
@@ -0,0 +1,42 @@
+// Questão 1 (feita em sala)
+
+
+
+
+// // Questão 2 ()
+// Desenvolver um programa que apresente no final a soma dos valores pares existentes na faixa de 0 até 500.
+// Utilize um laço que varie de 2 em 2.
+
+
+
+
+
+
+
+// let cost = 0;
+// let soma = 0;
+
+
+// do{ 
+    
+//     soma = soma + cost; // soma+= cost (cont de contador)
+    
+//     cost = cost +2;
+
+// }while(cost<=500);
+
+// console.log(`A soma doa valores pares de 0 a 500: ${soma}`);
+
+
+
+
+
+// Questão 3
+ 
+
+let 
+
+do{
+
+
+}while();
